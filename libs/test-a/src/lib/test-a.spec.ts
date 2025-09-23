338b95a463564c516f6414d2931a40d86d416352
Marcus Gilmore-Lim <marcus@clipboard.app>
2025-09-23 20:56:57 +1000

    Add ref to test-b in test-a

--- libs/test-a/src/lib/test-a.spec.ts
+++ libs/test-a/src/lib/test-a.spec.ts
@@ -1,5 +1,9 @@
 import { testA } from './test-a';
+import { testB } from 'test-b';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _testB = testB();
+console.log(_testB);
 describe('testA', () => {
   it('should work', () => {
     expect(testA()).toEqual('test-a');
